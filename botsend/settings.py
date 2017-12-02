f4eafac7d48b4147696dacbf8a45d7fb6457b33f
maximkolc <maximkolc@gmail.com>
2017-12-03 02:55:19 +0300

    static root добавил

--- botsend/settings.py
+++ botsend/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+#STATIC_ROOT = '/home/maximkolc/static'
 STATIC_URL = '/static/'
 # REDIS related settings 
 REDIS_HOST = 'localhost'
