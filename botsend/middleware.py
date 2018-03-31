f98e5cccdbec2aed335464b0f167a8589cab61f2
maximkolc <maximkolc@gmail.com>
2018-04-01 00:23:58 +0300

    без миграций3

--- botsend/middleware.py
+++ botsend/middleware.py
@@ -5,8 +5,11 @@ from django.utils.deprecation import MiddlewareMixin
 class SetLastVisitMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         if request.user.is_authenticated():
-            # Update last visit time after request finished processing.
-            p = Profile.objects.get(user=request.user)
-            p.last_visit=now()
-            p.save()
+            if Profile.objects.filter(user=request.user).exists()
+                # Update last visit time after request finished processing.
+                p = Profile.objects.get(user=request.user)
+                p.last_visit=now()
+                p.save()
+            else:
+                u = Profile(user = request.user, last_visit=now())
         return response 
